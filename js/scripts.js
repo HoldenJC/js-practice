6ac3a069972a8133bf7fcb611f572a95f857af0e
HoldenJC <holdenj_c@yahoo.com>
2019-06-03 10:53:59 -0700

    add Fahrenheit to Celcius converter practice scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Calculator to find user BMI to 3 digit accuracy
 var weight = prompt('weight:');
 var height = prompt('height:');
 
@@ -6,3 +7,15 @@ var bodyMI = function(weight, height) {
 }
 
 alert('Your BMI is: ' + bodyMI(weight, height).toPrecision(3));
+
+
+//Convert user input of degree Fahrenheit to Degree Celcius to 3 digit accuracy
+/*
+var tempF = prompt('Temperature in F you wish to convert to C:');
+
+var tempC = function(tempF) {
+	return (tempF - 32) * 5 / 9;
+}
+
+alert('Temp in C: ' + tempC(tempF).toPrecision(3));
+*/
